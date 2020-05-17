5ad7661a6e4ff6248591ab76a57f15931e2c64a6
nnmatiasc <nmatias@dc.uba.ar>
2020-05-17 20:05:47 -0300

    correcciones

--- Hotel/Habitacion.java
+++ Hotel/Habitacion.java
@@ -44,5 +44,24 @@ public class Habitacion {
     public void  setVecesOcupadaHabitacion(int VecesOcupadaHabitacionN){
         this.VecesOcupadaHabitacion=VecesOcupadaHabitacionN;
     }
+
+    /*
+
+        Hola, Palavecino!
+        Bien!
+
+        Recordar:
+        - los nombres de los atributos deben seguir la misma
+        regla de los métodos: si es una palabra sólo se escribe
+        con minúsculas y si tiene más de una palabra, la primera
+        debe comenzar con letra minúscula y las demás deben
+        comenzar con letra mayúscula.
+        - los paquetes deben tener nombres declarativos
+        - aplicar el concepto de alta cohesión en todas las clases
+        - asegurarse de que todos los trabajos compilen
+        - los nombres de las clases deben tener nombres
+        en singular
+
+     */
 }
 
